3ff61e9d73e8747ef88fa3fdc87b03474139c345
sahshad-jethur <shashad.p@jethur.com>
2026-01-17 16:56:59 +0530

    removed commented code

--- services/auth-service/cmd/main.go
+++ services/auth-service/cmd/main.go
@@ -1,34 +1,3 @@
-// package main
-
-// import (
-// 	"log"
-
-// 	"auth-service/internal/database"
-// 	"auth-service/internal/routes"
-
-// 	"github.com/gin-gonic/gin"
-// 	"github.com/joho/godotenv"
-// )
-
-// func main() {
-// 	if err := godotenv.Load(); err != nil {
-// 		log.Println("⚠️  No .env file found, using environment variables")
-// 	}
-
-// 	db, err := database.ConnectDB()
-// 	if err != nil {
-// 		log.Fatalf("Database connection failed: %v", err)
-// 	}
-
-// 	r := gin.Default()
-// 	routes.SetupRoutes(r, db)
-
-// 	log.Println("🚀 Auth service running on :8080")
-// 	if err := r.Run(":8080"); err != nil {
-// 		log.Fatalf("Server failed to start: %v", err)
-// 	}
-// }
-
 package main
 
 import (
